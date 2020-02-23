bec73f88198fd1f3d3638c03e4ff70a97d71f1b9
Will C <2willaddiction@gmail.com>
2020-02-22 22:31:35 -0500

    Add menu.html file in templates directory. render template in code replacing loop to output

--- vagrant/flaskProject.py
+++ vagrant/flaskProject.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -17,15 +17,7 @@ session = DBSession()
 def restaurantMenu(restaurant_id):
 	restaurant = session.query(Restaurant).first()
 	items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
-	output = ''
-	for i in items:
-		output += i.name
-		output += '</br>'
-		output += i.price
-		output += '</br>'
-		output += i.description
-		output += '</br></br>'
-	return output
+	return render_template('menu.html', restaurant=restaurant, items = items)
 
 @app.route('/restaurants/<int:restaurant_id>/new')
 def newMenuItem(restaurant_id):
